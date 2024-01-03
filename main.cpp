e128c6c291a983414e7b4bec6b030b6a043914a1
Zhalgas <zhalgas99@outlook.com>
2024-01-03 13:13:51 +0600

    first

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+import SomeModule;
+
+int main() {
+  std::cout << HelloWorld() << std::endl;
+  return 0;
+}
---
+++ some_module.cpp
@@ -0,0 +1,9 @@
+module;
+
+#include <string>
+
+export module SomeModule;
+
+export std::string HelloWorld() {
+  return "Hello, World!";
+}
